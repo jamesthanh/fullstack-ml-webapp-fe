4d9380d98459b2680cbafa3f2502724f2f118152
Thanh Nguyen <jamesnguyen1st@gmail.com>
2020-04-17 01:50:53 +0700

    first commit

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./FaceRecognition.css";
+
+const FaceRecognition = ({ imageUrl, box }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        {
+          <img
+            id="inputimage"
+            alt={""}
+            src={imageUrl}
+            width="500px"
+            height="auto"
+          />
+        }
+        {box.map((item, index) => (
+          <div
+            className="bounding-box"
+            style={{
+              top: item.topRow,
+              right: item.rightCol,
+              bottom: item.bottomRow,
+              left: item.leftCol,
+            }}
+            key={index}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default FaceRecognition;
